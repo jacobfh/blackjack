484f6e5551fdfc4418cf05d88512ffe90ce3b7f3
Jacobfh <jfh708@gmail.com>
2018-08-15 11:55:40 -0700

    card class

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,36 @@
+class Deck
+
+    attr_accessor :deck
+
+    SUITS = [ :diamond, :heart, :spade, :club ]
+    RANKS = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+
+    def initialize
+        @deck = []
+        new_deck
+    end
+
+    def new_deck
+        SUITS.each do |suit|
+            RANKS.map do |rank|
+                @deck.push (Card.new(rank, suit))
+            end
+        end 
+    end
+
+    def deal
+        @deck.shift
+    end
+
+    def shuffle
+        @deck.shuffle!
+    end
+
+    def cards_left
+        @deck.length
+    end
+end
+
 class Card
     attr_accessor :rank, :suit, :value
 
@@ -19,6 +52,9 @@ class Card
                 10
             when :K
                 10
+            else
+                 nil
             end
     end
 end
+
